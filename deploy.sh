8e8283f9eb2d9d396a1dc790e09177d6e250119d
Sergey Anisov <sanisov@tomsksoft.com>
2022-09-09 14:04:15 +0700

    deploy config

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+flutter pub get
+flutter analyze
+flutter test
+flutter build web --base-href /client/ --release
+cd build
+zip client.zip web/ -r
+scp -i ~/.ssh/id_rsa -P 2323 client.zip server-runner@mafia-generator.tomsk.ru:~/
+rm client.zip
+ssh -i ~/.ssh/id_rsa server-runner@mafia-generator.tomsk.ru -p 2323 "./update_client.sh"
