09f8e830aafca82547d3b2f3766258d21aeb1aad
Sergey Anisov <sanisov@tomsksoft.com>
2022-09-29 20:40:02 +0700

    use shared preferences instead of secure storage

--- lib/data/storages/token_storage_impl.dart
+++ lib/data/storages/token_storage_impl.dart
@@ -1,21 +1,22 @@
 import 'package:seating_generator_web/data/storages/token_storage.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenStorageImpl implements TokenStorage {
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  Future<SharedPreferences> get _storage => SharedPreferences.getInstance();
   static const _authTokenKey = "auth_token_key";
   static const _recoveryTokenKey = "recovery_token_key";
 
   @override
-  Future<String?> get authToken => _storage.read(key: _authTokenKey);
+  Future<String?> get authToken =>
+      _storage.then((value) => value.getString(_authTokenKey));
 
   @override
   Future onTokensUpdated(String authToken, String recoveryToken) async {
-    await _storage.write(key: _authTokenKey, value: authToken);
-    await _storage.write(key: _recoveryTokenKey, value: recoveryToken);
+    (await _storage).setString(_authTokenKey, authToken);
+    (await _storage).setString(_recoveryTokenKey, recoveryToken);
   }
 
   @override
-  Future<String?> get recoveryToken => _storage.read(key: _recoveryTokenKey);
-
+  Future<String?> get recoveryToken =>
+      _storage.then((value) => value.getString(_recoveryTokenKey));
 }
