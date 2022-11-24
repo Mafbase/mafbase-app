581cc8e1e973a8d7b50492ef52bb036e2d0d7e89
bansleep <levgalts123@gmail.com>
2022-11-24 18:03:37 +0700

    merge main into branch

--- lib/data/requests/verification_request.dart
+++ lib/data/requests/verification_request.dart
@@ -2,7 +2,7 @@ import 'package:seating_generator_web/data/base_request.dart';
 import 'package:seating_generator_web/seating-generator-proto/mafia.pb.dart';
 
 class VerificationRequest extends BaseRequest<EmailVerificationEventOut> {
-  VerificationRequest(EmailVerificationEvent event) : super('/api/emailVerification', event);
+  VerificationRequest(EmailVerificationEvent event) : super('/api/emailVerification', data: event);
 
   @override
   EmailVerificationEventOut parse(List<int> bytes) {
