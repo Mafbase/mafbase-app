c4e48f0e42df1864b30a4a4fd18dd8365aa42665
sergeyanisov <strelas70@gmail.com>
2023-12-01 23:14:18 +0700

    cleanup

--- lib/data/sockets/tournament_content_socket.dart
+++ lib/data/sockets/tournament_content_socket.dart
@@ -1,8 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:seating_generator_web/data/base_socket.dart';
 
 class TournamentContentSocket extends BaseSocket {
   TournamentContentSocket({required int tournamentId, required int table})
       : super(
-          "wss://mafbase.ru/api/seatingContent?table=$table&tournamentId=$tournamentId",
+          "${getScheme()}://${getHost()}/api/seatingContent?table=$table&tournamentId=$tournamentId",
         );
+}
+
+String getScheme() {
+  if (kIsWeb) {
+    return Uri.base.scheme == 'https' ? 'wss' : 'ws';
+  }
+
+  return 'wss';
+}
+
+String getHost() {
+  if (kIsWeb) {
+    return "${Uri.base.host}:${Uri.base.port}";
+  }
+
+  return "mafbase.ru";
 }
