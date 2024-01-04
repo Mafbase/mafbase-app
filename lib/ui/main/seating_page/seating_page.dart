14ebdb86081fc307e5cc9bb0fd340748b1073721
sergeyanisov <strelas70@gmail.com>
2024-01-04 18:22:29 +0500

    use pageView for mobile

--- lib/ui/main/seating_page/seating_page.dart
+++ lib/ui/main/seating_page/seating_page.dart
@@ -56,169 +56,125 @@ class _SeatingPageState extends State<SeatingPage> {
         builder: (context, state) {
           return Stack(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Text(
-                      tournamentState.isMyTournament
-                          ? context.locale.separateTitle
-                          : context.locale.seating,
-                      style: MyTheme.of(context).headerTextStyle,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    if (tournamentState.isMyTournament) ...[
-                      Container(
-                        constraints: const BoxConstraints(maxHeight: 100),
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: state.cannotMeet.length,
-                          itemBuilder: (context, index) {
-                            return Row(
-                              children: [
-                                    state.cannotMeet[index].first,
-                                    state.cannotMeet[index].second,
-                                  ]
-                                      .map<Widget>(
-                                        (playerModel) => Expanded(
-                                          child: Container(
-                                            decoration: BoxDecoration(
-                                              border: Border.all(
-                                                color: MyTheme.of(context)
-                                                    .borderColor,
-                                              ),
+              Column(
+                children: [
+                  Text(
+                    tournamentState.isMyTournament
+                        ? context.locale.separateTitle
+                        : context.locale.seating,
+                    style: MyTheme.of(context).headerTextStyle,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  if (tournamentState.isMyTournament) ...[
+                    Container(
+                      constraints: const BoxConstraints(maxHeight: 100),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: state.cannotMeet.length,
+                        itemBuilder: (context, index) {
+                          return Row(
+                            children: [
+                                  state.cannotMeet[index].first,
+                                  state.cannotMeet[index].second,
+                                ]
+                                    .map<Widget>(
+                                      (playerModel) => Expanded(
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                              color: MyTheme.of(context)
+                                                  .borderColor,
                                             ),
-                                            child: Center(
-                                              child: Padding(
-                                                padding:
-                                                    const EdgeInsets.all(8.0),
-                                                child: Text(
-                                                  playerModel.nickname,
-                                                  style: MyTheme.of(context)
-                                                      .defaultTextStyle,
-                                                ),
+                                          ),
+                                          child: Center(
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Text(
+                                                playerModel.nickname,
+                                                style: MyTheme.of(context)
+                                                    .defaultTextStyle,
                                               ),
                                             ),
                                           ),
                                         ),
-                                      )
-                                      .toList() +
-                                  <Widget>[
-                                    IconButton(
-                                      onPressed: () {
-                                        final bloc =
-                                            context.read<SeatingPageBloc>();
-                                        showDialog<bool>(
-                                          context: context,
-                                          builder: (context) =>
-                                              const ConfirmDialog(),
-                                        ).then(
-                                          (value) {
-                                            if (value == true) {
-                                              bloc.add(
-                                                SeatingPageEvent.deletePair(
-                                                  first: state
-                                                      .cannotMeet[index].first,
-                                                  second: state
-                                                      .cannotMeet[index].second,
-                                                ),
-                                              );
-                                            }
-                                          },
-                                        );
-                                      },
-                                      icon: Icon(
-                                        Icons.delete,
-                                        color: MyTheme.of(context).redColor,
                                       ),
+                                    )
+                                    .toList() +
+                                <Widget>[
+                                  IconButton(
+                                    onPressed: () {
+                                      final bloc =
+                                          context.read<SeatingPageBloc>();
+                                      showDialog<bool>(
+                                        context: context,
+                                        builder: (context) =>
+                                            const ConfirmDialog(),
+                                      ).then(
+                                        (value) {
+                                          if (value == true) {
+                                            bloc.add(
+                                              SeatingPageEvent.deletePair(
+                                                first: state
+                                                    .cannotMeet[index].first,
+                                                second: state
+                                                    .cannotMeet[index].second,
+                                              ),
+                                            );
+                                          }
+                                        },
+                                      );
+                                    },
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: MyTheme.of(context).redColor,
                                     ),
-                                  ],
-                            );
-                          },
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          context.read<SeatingPageBloc>().add(
-                                const SeatingPageEvent.addPair(),
-                              );
+                                  ),
+                                ],
+                          );
                         },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            context.locale.addSeparationBtnText,
-                          ),
-                        ),
-                      ),
-                    ],
-                    Expanded(
-                      flex: 100,
-                      child: SeatingList(
-                        models: state.games,
                       ),
                     ),
-                    if (tournamentState.isMyTournament)
-                      Wrap(
-                        children: [
-                          if (state.games.lastOrNull
-                                  ?.any((element) => element.gameWin != null) ==
-                              true)
-                            TextButton(
-                              onPressed: () {
-                                onSwissGameCreate(state, true);
-                              },
-                              child: const Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Сгенерировать следующий полуфинальный тур',
-                                ),
-                              ),
-                            )
-                          else
-                            TextButton(
-                              onPressed: () {
-                                ConfirmDialog.open(
-                                  context,
-                                  "Новая рассадка заменит старую",
-                                ).then((value) {
-                                  if (value == true) {
-                                    onSwissGameCreate(state, false);
-                                  }
-                                });
-                              },
-                              child: const Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Перегенерировать текущий полуфинальный тур',
-                                ),
-                              ),
-                            ),
-                          if (tournamentState.finalPlayers.length == 10)
-                            TextButton(
-                              onPressed: () {
-                                ConfirmDialog.open(
-                                  context,
-                                  "Новая рассадка заменит старую",
-                                ).then(
-                                  (value) {
-                                    if (value == true && context.mounted) {
-                                      context.read<SeatingPageBloc>().add(
-                                            const SeatingPageEvent
-                                                .createFinalSeating(),
-                                          );
-                                    }
-                                  },
-                                );
-                              },
-                              child: const Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  "Сгенерировать рассадку на финал",
-                                ),
+                    TextButton(
+                      onPressed: () {
+                        context.read<SeatingPageBloc>().add(
+                              const SeatingPageEvent.addPair(),
+                            );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          context.locale.addSeparationBtnText,
+                        ),
+                      ),
+                    ),
+                  ],
+                  Expanded(
+                    flex: 100,
+                    child: SeatingList(
+                      models: state.games,
+                    ),
+                  ),
+                  if (tournamentState.isMyTournament)
+                    Wrap(
+                      children: [
+                        if (state.games.lastOrNull
+                                ?.any((element) => element.gameWin != null) ==
+                            true)
+                          TextButton(
+                            onPressed: () {
+                              onSwissGameCreate(state, true);
+                            },
+                            child: const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Сгенерировать следующий полуфинальный тур',
                               ),
                             ),
+                          )
+                        else
                           TextButton(
                             onPressed: () {
                               ConfirmDialog.open(
@@ -226,37 +182,78 @@ class _SeatingPageState extends State<SeatingPage> {
                                 "Новая рассадка заменит старую",
                               ).then((value) {
                                 if (value == true) {
-                                  if (!mounted) return;
-                                  context.read<SeatingPageBloc>().add(
-                                        const SeatingPageEvent.createSeating(),
-                                      );
+                                  onSwissGameCreate(state, false);
                                 }
                               });
                             },
                             child: const Padding(
                               padding: EdgeInsets.all(8.0),
                               child: Text(
-                                "Сгенерировать рассадку",
+                                'Перегенерировать текущий полуфинальный тур',
                               ),
                             ),
                           ),
+                        if (tournamentState.finalPlayers.length == 10)
                           TextButton(
                             onPressed: () {
-                              context.read<SeatingPageBloc>().add(
-                                    const SeatingPageEvent.fsmSeatingTapped(),
-                                  );
+                              ConfirmDialog.open(
+                                context,
+                                "Новая рассадка заменит старую",
+                              ).then(
+                                (value) {
+                                  if (value == true && context.mounted) {
+                                    context.read<SeatingPageBloc>().add(
+                                          const SeatingPageEvent
+                                              .createFinalSeating(),
+                                        );
+                                  }
+                                },
+                              );
                             },
                             child: const Padding(
                               padding: EdgeInsets.all(8.0),
                               child: Text(
-                                "Загрузить готовую рассадку",
+                                "Сгенерировать рассадку на финал",
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                  ],
-                ),
+                        TextButton(
+                          onPressed: () {
+                            ConfirmDialog.open(
+                              context,
+                              "Новая рассадка заменит старую",
+                            ).then((value) {
+                              if (value == true) {
+                                if (!mounted) return;
+                                context.read<SeatingPageBloc>().add(
+                                      const SeatingPageEvent.createSeating(),
+                                    );
+                              }
+                            });
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Сгенерировать рассадку",
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            context.read<SeatingPageBloc>().add(
+                                  const SeatingPageEvent.fsmSeatingTapped(),
+                                );
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Загрузить готовую рассадку",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
               ),
               if (state.isLoading)
                 const Positioned.fill(child: LoadingOverlayWidget()),
--- lib/ui/main/seating_page/widgets/seating_list.dart
+++ lib/ui/main/seating_page/widgets/seating_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:seating_generator_web/common/bloc_extension.dart';
 import 'package:seating_generator_web/common/widgets/game_result_widget.dart';
@@ -15,43 +17,86 @@ class SeatingList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: models.length,
-      itemBuilder: (context, columnIndex) {
-        return LayoutBuilder(
-          builder: (context, layout) => SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              constraints: BoxConstraints(
-                minWidth: layout.maxWidth,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final coef = min(
+          min(
+            (constraints.maxHeight / 552),
+            constraints.maxWidth / 432,
+          ),
+          1.0,
+        );
+
+        final pageView = coef < .99;
+
+        Widget itemBuilder(BuildContext context, int columnIndex) {
+          final children = models[columnIndex].map((model) {
+            return Transform.scale(
+              scale: coef,
+              child: InkWell(
+                onTap: () {
+                  context.read<SeatingPageBloc>().add(
+                        SeatingPageEvent.openGameEditing(
+                          gameId: model.gameId,
+                        ),
+                      );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: GameResultWidget(
+                    model: model,
+                  ),
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: models[columnIndex].map((model) {
-                  return InkWell(
-                    onTap: () {
-                      context.read<SeatingPageBloc>().add(
-                            SeatingPageEvent.openGameEditing(
-                              gameId: model.gameId,
-                            ),
-                          );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: GameResultWidget(
-                        model: model,
+            );
+          }).toList();
+          if (pageView) {
+            return SizedBox(
+              height: constraints.maxHeight,
+              child: PageView(
+                children: children
+                    .map(
+                      (e) => Center(
+                        child: e,
                       ),
-                    ),
-                  );
-                }).toList(),
+                    )
+                    .toList(),
+              ),
+            );
+          }
+
+          return LayoutBuilder(
+            builder: (context, layout) => SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                constraints: BoxConstraints(
+                  minWidth: layout.maxWidth,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                ),
               ),
             ),
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return const SizedBox(
-          height: 8,
+          );
+        }
+
+        if (pageView) {
+          return PageView.builder(
+            itemCount: models.length,
+            itemBuilder: itemBuilder,
+            scrollDirection: Axis.vertical,
+          );
+        }
+
+        return ListView.separated(
+          itemCount: models.length,
+          itemBuilder: itemBuilder,
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(
+              height: 8,
+            );
+          },
         );
       },
     );
