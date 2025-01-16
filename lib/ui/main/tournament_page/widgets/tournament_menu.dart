5a529b2f8b2c662b5076e766df180cd19a99cd69
Анисов Сергей <strelas70@gmail.com>
2025-01-16 16:34:26 +0500

    fix tournament menu

--- lib/ui/main/tournament_page/widgets/tournament_menu.dart
+++ lib/ui/main/tournament_page/widgets/tournament_menu.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:seating_generator_web/app/assets.dart';
 import 'package:seating_generator_web/common/theme/my_theme.dart';
 
 class TournamentMenu extends StatefulWidget {
@@ -17,75 +15,31 @@ class TournamentMenu extends StatefulWidget {
 
 class _TournamentMenuState extends State<TournamentMenu> {
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 380,
-      child: Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: MyTheme.of(context).darkGreyColor,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: widget.items.map(
-                    (e) {
-                      return Material(
-                        color: MyTheme.of(context).darkGreyColor,
-                        child: InkWell(
-                          onTap: e.onTap,
-                          child: SizedBox(
-                            height: 64,
-                            child: Center(
-                              child: Text(
-                                e.text,
-                                style: MyTheme.of(context).btnTextStyle,
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ).toList(),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 330,
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          color: MyTheme.of(context).redColor,
-                          child: Center(
-                            child: SvgPicture.asset(AppAssets.logoAsset),
-                          ),
+  Widget build(BuildContext context) => Container(
+        width: 380,
+        color: MyTheme.of(context).darkGreyColor,
+        child: Column(
+          children: widget.items
+              .map(
+                (e) => Material(
+                  color: MyTheme.of(context).darkGreyColor,
+                  child: InkWell(
+                    onTap: e.onTap,
+                    child: SizedBox(
+                      height: 64,
+                      child: Center(
+                        child: Text(
+                          e.text,
+                          style: MyTheme.of(context).btnTextStyle,
                         ),
                       ),
-                      Container(
-                        height: 42,
-                        color: MyTheme.of(context).darkBlueColor,
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    color: MyTheme.of(context).greyColor,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+              )
+              .toList(),
+        ),
+      );
 }
 
 class MenuItemModel {
