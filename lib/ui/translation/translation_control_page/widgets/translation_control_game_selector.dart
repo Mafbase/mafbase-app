388fa686784e1ef76c53212a44ec1a4da6566733
Анисов Сергей <strelas70@gmail.com>
2026-04-06 22:45:29 +0500

    manual fix

--- lib/ui/translation/translation_control_page/widgets/translation_control_game_selector.dart
+++ lib/ui/translation/translation_control_page/widgets/translation_control_game_selector.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:seating_generator_web/common/theme/my_theme.dart';
+import 'package:seating_generator_web/common/widgets/custom_dropdown.dart';
 import 'package:seating_generator_web/utils.dart';
 
 class TranslationControlGameSelector extends StatelessWidget {
@@ -22,35 +23,16 @@ class TranslationControlGameSelector extends StatelessWidget {
       height: 52,
       color: theme.background2,
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          DropdownButton<int>(
-            value: game,
-            dropdownColor: theme.background2,
-            underline: const SizedBox.shrink(),
-            style: theme.defaultTextStyle.copyWith(
-              fontWeight: FontWeight.w600,
-              fontSize: 16,
-            ),
-            items: List.generate(
-              totalGames,
-              (i) => DropdownMenuItem<int>(
-                value: i + 1,
-                child: Text(
-                  context.locale.translationControlGame(i + 1, totalGames),
-                  style: theme.defaultTextStyle.copyWith(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ),
-            onChanged: (value) {
-              if (value != null) onChanged(value);
-            },
-          ),
-        ],
+      child: CustomDropdown(
+        mapToString: (game) => 'Игра $game',
+        items: List.generate(
+          totalGames,
+          (i) => i + 1,
+        ),
+        initValue: game,
+        onChanged: (value) {
+          if (value != null) onChanged(value);
+        },
       ),
     );
   }
